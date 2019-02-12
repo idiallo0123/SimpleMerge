9a7fd4c33b0e68ecfa9086239a8b3d05fe59d632
idiallo0123 <idiallo0123@bths.edu>
2019-02-12 15:22:13 -0500

    ddrtgrr6

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -4,9 +4,41 @@
  * integers and merges them to return one sorted
  * array of integers.
  */
-public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+public class SimpleMerge
+{
 
+    public static int[] simpleMerge(int[] arr1, int[] arr2)
+    {
+        int a=0,b=0,n=0;
+        int[]arr=new int[arr1.length+arr2.length];
+        while(a<arr1.length&&b<arr2.length){
+            if(arr1[a]<arr2[b]){
+                arr[n]=arr1[a];
+                n++;
+                a++;
+            }
+            else
+                {
+                arr[n]=arr2[b];
+                n++;
+                b++;
+            }
+        }
+        if(a>b){
+            while(b<arr2.length){
+                arr[n]=arr2[b];
+                n++;
+                b++;
+            }
+        }else{
+            while (a<arr1.length){
+                arr[n]=arr1[a];
+                n++;
+                a++;
+            }
+        }
+        return arr;
     }
 
 }
+
